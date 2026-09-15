69e46f6be7c723b3f7a57a1e9ff7abe6063e61cf
Trương Mạnh Đạt <truongmanhdat061@gmail.com>
2026-09-15 20:40:10 +0700

    Implement operator overloading for DaThuc class

--- C_to_Cpp/Day21/Bai2./bai2.cpp
+++ C_to_Cpp/Day21/Bai2./bai2.cpp
@@ -15,6 +15,8 @@ class DaThuc{
 		friend istream& operator>>(istream &in, DaThuc &dt);
 		friend ostream& operator<<(ostream &out, const DaThuc &dt);
 		
+		DaThuc operator+(const DaThuc &dtk) const;
+		DaThuc operator*(const DaThuc &dtk)	const;
 };
 
 
@@ -36,6 +38,8 @@ istream& operator>>(istream &in, DaThuc &dt){
 		cout << "Nhap he so bac " << i << ":";
 		in >> dt.heSo[i];
 	}
+	
+	return in;
 }
 
 ostream& operator<<(ostream &out, const DaThuc &dt){
@@ -45,25 +49,53 @@ ostream& operator<<(ostream &out, const DaThuc &dt){
 		if(dt.heSo[i] == 0)	continue;
 		
 		if(soDaIn == 0){
-			if(dt.heSo[i] < 0)	cout << "-";
+			if(dt.heSo[i] < 0)	out << "-";
 		}
 		else{
-			if(dt.heSo[i] < 0)	cout << " - ";
-			else				cout << " + ";
+			if(dt.heSo[i] < 0)	out << " - ";
+			else				out << " + ";
 		}
 		
-		if(dt.heSo[i] != 1 || i == 0)	cout << fixed << setprecision(2) << abs(dt.heSo[i]);
-		if(i > 1)						cout << "x^" << i;
-		else if(i == 1)					cout << "x";
+		if(dt.heSo[i] != 1 || i == 0)	out << fixed << setprecision(2) << abs(dt.heSo[i]);
+		if(i > 1)						out << "x^" << i;
+		else if(i == 1)					out << "x";
 		
 		soDaIn++;
 	}
-	if(soDaIn == 0)		cout << 0;
+	if(soDaIn == 0)		out << 0;
+	
+	return out;
 }
 
+DaThuc DaThuc::operator+(const DaThuc &dtk) const{
+	int bacTong = (this->bac > dtk.bac) ? this->bac : dtk.bac;
+	
+	DaThuc kq(bacTong);
+	
+	for(int i=0; i<=kq.bac; i++){
+		float hs1 = (i <= this->bac) ? this->heSo[i] : 0;
+		float hs2 = (i <= dtk.bac)   ? dtk.heSo[i]   : 0;
+		
+		kq.heSo[i] = hs1 + hs2;
+	}
+	
+	while(kq.heSo[kq.bac] == 0)	kq.bac--;
+	
+	return kq;
+}
 
-
-
+DaThuc DaThuc::operator*(const DaThuc &dtk) const{
+	int bacTich = bac * dtk.bac;
+	DaThuc kq(bacTich);
+	
+	for(int i=0 ; i<=bac; i++){
+		for(int j=0; j<=dtk.bac; j++){
+			kq.heSo[i+j] += heSo[i] * dtk.heSo[j];	
+		}
+	}
+	
+	return kq;
+}
 
 
 
@@ -82,7 +114,10 @@ ostream& operator<<(ostream &out, const DaThuc &dt){
 
 
 int main(){
-	DaThuc dt;
-	cin >> dt;
-	cout << "\nDa thuc vua nhap:" << dt;
+	DaThuc dt1, dt2;
+	cin >> dt1 >> dt2;
+	
+	cout << dt1 + dt2 << endl;
+	cout << dt1 * dt2 << endl;
+	
 }
