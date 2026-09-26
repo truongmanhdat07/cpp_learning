e4e3515eb78657bd9ea06270acf8d679783327cc
Trương Mạnh Đạt <truongmanhdat061@gmail.com>
2026-09-26 08:43:37 +0700

    Add student class and management functions
    Implement student management system with input/output functions.

---
+++ Data_Structures_and_Algorithms/Homework/III_2/Vector/bai3.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+#include "myVector.h"
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+class SinhVien{
+	private:
+		string sbd = "";
+		string hoTen = "";
+		int namSinh = 0;
+		string gioiTinh = "";
+		float diem = 0.0f;
+	public:
+		SinhVien(){};
+		SinhVien(string sbd, string hoTen, int namSinh, string gioiTinh, float diem);
+		
+		string getSbd();
+		
+		friend istream& operator>>(istream &is, SinhVien &sv);
+		friend ostream& operator<<(ostream &os, SinhVien &sv);
+};
+
+
+void nhapDanhSach(istream &is, Vector<SinhVien> &danhSach, int soLuong);
+void nhapFile(Vector<SinhVien> &danhSach, string tenFileInput);
+void inTieuDe(ostream &os);
+void xuatDanhSach(ostream &os, Vector<SinhVien> &danhSach);
+void xuatFile(Vector<SinhVien> &danhSach, string tenFileOutput);
+void boSungCuoi(Vector<SinhVien> &danhSach);
+void xoaSinhVien(Vector<SinhVien> &danhSach, string soBaoDanh);
+void xoaSinhVienTheoSBD(Vector<SinhVien> &danhSach);
+
+
+
+SinhVien::SinhVien(string sbd, string hoTen, int namSinh, string gioiTinh, float diem){
+	this->sbd = sbd;
+	this->hoTen = hoTen;
+	this->namSinh = namSinh;
+	this->gioiTinh = gioiTinh;
+	this->diem = diem;
+}
+
+string SinhVien::getSbd(){
+	return sbd;
+}
+
+istream& operator>>(istream &is, SinhVien &sv){
+	if(&is == &cin){
+		cout << "Nhap so bao danh:";		getline(is, sv.sbd);
+		cout << "Nhap ho ten:";				getline(is, sv.hoTen);
+		cout << "Nhap nam sinh:";			is >> sv.namSinh;	is.ignore();
+		cout << "Nhap gioi tinh:";			getline(is, sv.gioiTinh);
+		cout << "Nhap diem:";				is >> sv.diem;		is.ignore();
+	}
+	else{
+		getline(is, sv.sbd);
+		getline(is, sv.hoTen);
+		is >> sv.namSinh;	is.ignore();
+		getline(is, sv.gioiTinh);
+		is >> sv.diem;		is.ignore();		
+	}
+	
+	return is;
+}
+
+ostream& operator<<(ostream &os, SinhVien &sv){
+	os	<< left << fixed << setprecision(2)
+		<< setw(12) << sv.sbd
+		<< setw(25) << sv.hoTen
+		<< setw(12) << sv.namSinh
+		<< setw(12) << sv.gioiTinh
+		<< setw(8)  << sv.diem << endl;
+		
+	return os;
+}
+
+
+
+int main(){
+	int soLuong;
+	ifstream fileIn("sinhvien.txt");
+	fileIn >> soLuong;	fileIn.ignore();
+	
+	Vector<SinhVien> danhSach(soLuong);
+	
+	nhapDanhSach(fileIn, danhSach, soLuong);
+	
+	ofstream fileOut("ketqua.txt");
+	xuatDanhSach(fileOut, danhSach);
+	
+	boSungCuoi(danhSach);
+	
+	xoaSinhVienTheoSBD(danhSach);
+	
+	return 0;
+}
+
+
+void nhapDanhSach(istream &is, Vector<SinhVien> &danhSach, int soLuong){
+	for(int i=0; i<soLuong; i++){
+		if(&is == &cin)		cout << "Nhap thong tin sinh vien thu " << i+1 << ":";
+		is >> danhSach[i];
+	}
+}
+
+
+void inTieuDe(ostream &os){
+	os	<< left 
+		<< setw(12) << "So Bao Danh"
+		<< setw(25) << "Ho Va Ten"
+		<< setw(12) << "Nam Sinh"
+		<< setw(12) << "Gioi tinh"
+		<< setw(8)  << "Diem" << endl;
+}
+
+void xuatDanhSach(ostream &os, Vector<SinhVien> &danhSach){
+	inTieuDe(os);
+	for(auto it = danhSach.begin(); it != danhSach.end(); it++){
+		os << *it ;
+	}
+}
+
+void boSungCuoi(Vector<SinhVien> &danhSach){
+	SinhVien sv;
+	cout << "\nNhap thong tin sinh vien bo sung cuoi danh sach:" << endl;
+	cin >> sv;
+	danhSach.push_back(sv);
+}
+
+void xoaSinhVien(Vector<SinhVien> &danhSach, string soBaoDanh){
+	bool timThay = false;
+	
+	for(auto it = danhSach.begin() ; it != danhSach.end(); it++){
+		if(it->getSbd() == soBaoDanh){
+			int viTri = it - danhSach.begin();
+			danhSach.erase(viTri);
+			timThay = true;
+			break;
+		}
+	}
+	if(timThay)	cout << "\nXoa thanh cong sinh vien co so bao danh:" << soBaoDanh;
+	else		cout << "\nKhong tim thay sinh vien co so bao danh:" << soBaoDanh;
+}
+
+void xoaSinhVienTheoSBD(Vector<SinhVien> &danhSach){
+	string soBaoDanh;
+	cout << "\nNhap so bao danh can xoa:";		getline(cin, soBaoDanh);
+	xoaSinhVien(danhSach, soBaoDanh);
+}
